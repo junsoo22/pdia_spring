f519b4f72dce1a855b16f7da0c0740bdedb14386
kim junsoo <kjs201105@naver.com>
2026-04-21 15:33:30 +0900

    model로 분리해서 메서드 호출하기

---
+++ src/main/java/com/example/demo/product/Product.java
@@ -0,0 +1,10 @@
+package com.example.demo.product;
+
+public class Product {
+
+    private String product="sneakers";
+
+    public String getProduct(){
+        return product;
+    }
+}
--- src/main/java/com/example/demo/product/ProductController.java
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -1,23 +1,20 @@
 package com.example.demo.product;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
+    Product products=new Product();
+
     ProductController() {
         System.out.println("초기화 중임");
     }
 
-
     //전체 상품 조회
     @GetMapping("/products")
     public String getProducts(){
-        return "sneakers";
+        return products.getProduct();
     }
 }
