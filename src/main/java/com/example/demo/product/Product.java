fdf49d8655e54d33d769d4ae7eaafcf16e01ca30
kim junsoo <kjs201105@naver.com>
2026-04-21 15:42:21 +0900

    model 객체 생성->model 메소드 호출

--- src/main/java/com/example/demo/product/Product.java
+++ src/main/java/com/example/demo/product/Product.java
@@ -1,5 +1,9 @@
 package com.example.demo.product;
 
+
+import org.springframework.stereotype.Component;
+
+@Component     //new Product()로 객체 생성하지 말고 스프링이 객체 생성 해주도록
 public class Product {
 
     private String product="sneakers";
--- src/main/java/com/example/demo/product/ProductController.java
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
 
-    Product products=new Product();
+    // Controller 태어날 때마다 product가 같이 태어남... 종속되어서 태어남.
+    // -> 객체 생성을 스프링이 해주도록 만들기 -> Product 모델에 @Component
+    Product products;
 
-    ProductController() {
-        System.out.println("초기화 중임");
+    ProductController(Product products) {
+        this.products=products;    //자바처럼 생성자에 초기화해줌.
     }
 
     //전체 상품 조회
