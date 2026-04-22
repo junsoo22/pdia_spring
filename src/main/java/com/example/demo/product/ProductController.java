4ca5691479ffb4a226dec8f0e38a64dfe7ba11f3
kim junsoo <kjs201105@naver.com>
2026-04-22 09:16:35 +0900

    필드에서 의존성 주입

--- src/main/java/com/example/demo/product/ProductController.java
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -1,14 +1,14 @@
 package com.example.demo.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
-    // Controller 태어날 때마다 product가 같이 태어남... 종속되어서 태어남.
-    // -> 객체 생성을 스프링이 해주도록 만들기 -> Product 모델에 @Component
-    Product products;
+    @Autowired
+    private Product products;
 
     ProductController(Product products) {
         this.products=products;    //자바처럼 생성자에 초기화해줌.
